b2a1dc691a9c8e08ace997e0448223ecc5d0086e
aron <arongao123@gmail.com>
2024-07-29 09:15:34 +0800

    Version 1.8

--- HistoryWindow.py
+++ HistoryWindow.py
@@ -1,5 +1,6 @@
 import json
 import ast
+import astor
 from difflib import SequenceMatcher
 from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QListWidget, QTextEdit
 
@@ -42,12 +43,7 @@ class HistoryWindow(QMainWindow):
         content1 = read_file(base_file)
         content2 = read_file(compare_file)
         similar_blocks, lines1, lines2 = find_similar_blocks(content1, content2)
-        extended_blocks = []
-        for block1_idx, block2_idx, similarity in similar_blocks:
-            length1, length2 = try_expand_block(lines1, lines2, block1_idx, block2_idx, 0.9)
-            length = min(length1, length2)
-            extended_blocks.append((block1_idx, block2_idx, length))
-        highlighted_content1, highlighted_content2 = highlight_code(extended_blocks, lines1, lines2)
+        highlighted_content1, highlighted_content2 = highlight_code(content1, content2, similar_blocks, lines1, lines2)
         self.show_diff(highlighted_content1, highlighted_content2)
 
     def show_diff(self, highlighted_content1, highlighted_content2):
@@ -76,26 +72,58 @@ def read_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
 
+def normalize_variable_names(node):
+    class VariableNameNormalizer(ast.NodeTransformer):
+        def __init__(self):
+            self.variable_map = {}
+            self.counter = 0
+
+        def get_new_name(self, original_name):
+            if original_name not in self.variable_map:
+                self.counter += 1
+                self.variable_map[original_name] = f"var{self.counter}"
+            return self.variable_map[original_name]
+
+        def visit_Name(self, node):
+            if isinstance(node.ctx, ast.Store) or isinstance(node.ctx, ast.Load):
+                new_name = self.get_new_name(node.id)
+                return ast.copy_location(ast.Name(id=new_name, ctx=node.ctx), node)
+            return node
+
+        def visit_arg(self, node):
+            new_name = self.get_new_name(node.arg)
+            return ast.copy_location(ast.arg(arg=new_name, annotation=node.annotation), node)
+
+    return VariableNameNormalizer().visit(node)
+
 def node_to_string(node):
-    return ast.dump(node)
+    return astor.to_source(node)
 
 def calculate_node_similarity(block1, block2):
     return SequenceMatcher(None, block1, block2).ratio()
 
+def ast_similarity(node1, node2):
+    normalized_node1 = normalize_variable_names(node1)
+    normalized_node2 = normalize_variable_names(node2)
+    return calculate_node_similarity(node_to_string(normalized_node1), node_to_string(normalized_node2))
+
 def find_similar_blocks(content1, content2, threshold=0.9):
-    lines1 = content1.splitlines()
-    lines2 = content2.splitlines()
+    tree1 = ast.parse(content1)
+    tree2 = ast.parse(content2)
+
+    blocks1 = [node for node in ast.walk(tree1) if isinstance(node, ast.stmt)]
+    blocks2 = [node for node in ast.walk(tree2) if isinstance(node, ast.stmt)]
 
     similar_blocks = []
     used_blocks2 = set()
 
-    for i, line1 in enumerate(lines1):
+    for i, line1 in enumerate(blocks1):
         best_match = None
         best_similarity = 0
-        for j, line2 in enumerate(lines2):
+        for j, line2 in enumerate(blocks2):
             if j in used_blocks2:
                 continue
-            similarity = calculate_node_similarity(line1, line2)
+            similarity = ast_similarity(line1, line2)
             if similarity >= threshold and similarity > best_similarity:
                 best_similarity = similarity
                 best_match = j
@@ -103,33 +131,23 @@ def find_similar_blocks(content1, content2, threshold=0.9):
             similar_blocks.append((i, best_match, best_similarity))
             used_blocks2.add(best_match)
 
-    return similar_blocks, lines1, lines2
+    return similar_blocks, blocks1, blocks2
 
-def try_expand_block(lines1, lines2, block1_start, block2_start, threshold):
-    block1_end = block1_start + 1
-    block2_end = block2_start + 1
-
-    while block1_end < len(lines1) and block2_end < len(lines2):
-        block1 = "\n".join(lines1[block1_start:block1_end + 1])
-        block2 = "\n".join(lines2[block2_start:block2_end + 1])
-
-        similarity = calculate_node_similarity(block1, block2)
-        if similarity >= threshold:
-            block1_end += 1
-            block2_end += 1
-        else:
-            break
-
-    return block1_end - block1_start, block2_end - block2_start
-
-def highlight_code(similar_blocks, lines1, lines2):
+def highlight_code(content1, content2, similar_blocks, blocks1, blocks2):
+    lines1 = content1.splitlines()
+    lines2 = content2.splitlines()
     highlighted_lines1 = lines1[:]
     highlighted_lines2 = lines2[:]
 
     for block1_idx, block2_idx, length in similar_blocks:
-        for i in range(block1_idx, block1_idx + length):
-            highlighted_lines1[i] = f'<span style="background-color: yellow">{lines1[i]}</span>'
-        for i in range(block2_idx, block2_idx + length):
-            highlighted_lines2[i] = f'<span style="background-color: yellow">{lines2[i]}</span>'
+        block1 = blocks1[block1_idx]
+        block2 = blocks2[block2_idx]
+
+        for i in range(block1.lineno - 1, block1.end_lineno):
+            if not lines1[i].strip().startswith("#"):
+                highlighted_lines1[i] = f'<span style="background-color: yellow">{lines1[i]}</span>'
+        for i in range(block2.lineno - 1, block2.end_lineno):
+            if not lines2[i].strip().startswith("#"):
+                highlighted_lines2[i] = f'<span style="background-color: yellow">{lines2[i]}</span>'
 
     return '<br>'.join(highlighted_lines1), '<br>'.join(highlighted_lines2)
---
+++ test/test4.py
@@ -0,0 +1,19 @@
+def add(a, b):
+    # This function adds two numbers
+    # This function adds two numbers
+    # This function adds two numbers
+    return a + b
+def subtract(a, b):
+    # This function subtracts the second number from the first
+    # This function subtracts the second number from the first
+    # This function subtracts the second number from the first
+    return a - b
+    # This function subtracts the second number from the first
+    # This function subtracts the second number from the first
+    # This function subtracts the second number from the first
+    # This function subtracts the second number from the first
+    # This function subtracts the second number from the first
+    # This function subtracts the second number from the first
+    # This function subtracts the second number from the first
+    # This function subtracts the second number from the first
+    # This function subtracts the second number from the first
