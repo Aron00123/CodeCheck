5a88d2cc4afe3caa31bfa237ca7a45354b8124cb
TripleCamera <TripleCamera@outlook.com>
2024-07-29 17:50:05 +0800

    在任务栏上显示自定义图标，增大图标尺寸

--- Pyqt5.py
+++ Pyqt5.py
@@ -1,8 +1,12 @@
+import ctypes
 import sys
 from LoginWindow import LoginWindow
 from PyQt5.QtWidgets import QApplication
 
 def main():
+    # 为程序设置独立的 ID，让任务栏图标能够正常显示
+    # https://stackoverflow.com/a/1552105/20025220
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID('codecheck')
     app = QApplication(sys.argv)
     login_window = LoginWindow()
     login_window.show()
